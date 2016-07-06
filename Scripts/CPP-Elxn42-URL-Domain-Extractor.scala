a8b6f9b7db856fabc09be7032d275476cb8093e6
ianmilligan1 <i2milligan@uwaterloo.ca>
2016-07-06 08:44:32 -0400

    URL and Domain Counts

---
+++ Scripts/CPP-Elxn42-URL-Domain-Extractor.scala
@@ -0,0 +1,39 @@
+import org.warcbase.spark.matchbox._
+import org.warcbase.spark.rdd.RecordRDD._
+
+/** CPP **/
+
+RecordLoader.loadArchives("/mnt/vol1/data_sets/cpp/cpp_warcs_accession_02/*201508*.gz", sc)
+  .keepValidPages()
+  .map(r => r.getUrl)
+  .saveAsTextFile("/mnt/vol1/derivative_data/cpp-urls-201508")
+
+RecordLoader.loadArchives("/mnt/vol1/data_sets/cpp/cpp_warcs_accession_02/*201508*.gz", sc)
+  .keepValidPages() 
+  .map(r => ExtractDomain(r.getUrl)) 
+  .countItems() 
+  .saveAsTextFile("/mnt/vol1/derivative_data/cpp-counted-domains-201508")
+
+RecordLoader.loadArchives("/mnt/vol1/data_sets/cpp/cpp_warcs_accession_02/*201511*.gz", sc)
+  .keepValidPages()
+  .map(r => r.getUrl)
+  .saveAsTextFile("/mnt/vol1/derivative_data/cpp-urls-201511")
+
+RecordLoader.loadArchives("/mnt/vol1/data_sets/cpp/cpp_warcs_accession_02/*201511*.gz", sc)
+  .keepValidPages() 
+  .map(r => ExtractDomain(r.getUrl)) 
+  .countItems() 
+  .saveAsTextFile("/mnt/vol1/derivative_data/cpp-counted-domains-201511")
+
+/** ELXN42 **/
+
+RecordLoader.loadArchives("/mnt/vol1/data_sets/elxn42/warcs/*.gz", sc)
+  .keepValidPages()
+  .map(r => r.getUrl)
+  .saveAsTextFile("/mnt/vol1/derivative_data/elxn42-urls-201601")
+
+RecordLoader.loadArchives("/mnt/vol1/data_sets/elxn42/warcs/*.gz", sc)
+  .keepValidPages() 
+  .map(r => ExtractDomain(r.getUrl)) 
+  .countItems() 
+  .saveAsTextFile("/mnt/vol1/derivative_data/elxn42-counted-domains-201601")
