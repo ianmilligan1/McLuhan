0bc4b4339c03377bc4228c3c293663d27abbdeae
ianmilligan1 <i2milligan@uwaterloo.ca>
2016-08-04 08:00:18 -0400

    major reorg, checking in files

---
+++ NER/markup/scp.sh
@@ -0,0 +1,3 @@
+scp *.txt i2millig@rho.library.yorku.ca:/home/i2millig/McLuhan-NER
+scp *pattr i2millig@rho.library.yorku.ca:/home/i2millig/McLuhan-NER
+scp Stanford-NER-Bash.sh i2millig@rho.library.yorku.ca:/home/i2millig/McLuhan-NER
